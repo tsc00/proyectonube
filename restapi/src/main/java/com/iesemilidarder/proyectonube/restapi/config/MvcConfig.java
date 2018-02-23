b6e2551ef094aeacb30c41a7b2491cc479d068cb
Tomás Sastre <32461851+tsc00@users.noreply.github.com>
2018-02-23 20:50:54 +0100

    Update MvcConfig.java

--- restapi/src/main/java/com/iesemilidarder/proyectonube/restapi/config/MvcConfig.java
+++ restapi/src/main/java/com/iesemilidarder/proyectonube/restapi/config/MvcConfig.java
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * Created by Tomás in 22/2/2018
  * Description:
  **/
+//Esta clase se utiliza para decir que url va a cada pagina
 
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
